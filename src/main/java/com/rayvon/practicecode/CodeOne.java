1c482b0a27c3f81aca7f08c5be6fc5e12f075aae
RayRoyal76 <72403835+RayRoyal76@users.noreply.github.com>
2020-11-14 12:09:00 -0500

    first commit

---
+++ src/main/java/com/rayvon/practicecode/CodeOne.java
@@ -0,0 +1,21 @@
+package com.rayvon.practicecode;
+
+public class CodeOne {
+    // main method - entry point
+    public static void main(String[] args) {
+        // simple System.out statement
+      //  System.out.println("It worked");
+
+    // variable
+    int nums = -12;
+    // if statement
+    if(nums > 0) {
+        System.out.println("We Up!");
+    }// added else statement
+    else{
+        System.out.println("we need more!");
+    }
+
+}
+
+}
